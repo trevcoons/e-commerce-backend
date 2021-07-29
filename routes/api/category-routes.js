3cff517ae9bfbec2e28201e0055862662b88ffde
twizard <trevcoons@gmail.com>
2021-07-28 19:45:17 -0700

    add delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -84,6 +84,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCatData => {
+      if (!dbCatData){
+        res.status(404).json({message: 'No category found with that id.'});
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
